96745529117ede26fae4a12e9d772f7ceac39aca
Alex Rolfe <rolfepercussion@gmail.com>
2016-12-07 18:34:13 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {fetchData} from './lib/components/Api.js';
 
 class App extends Component {
+
+  // fetchData in componentDidMount
+  // create a componen list reps
   render() {
+    fetchData();
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Congress Frontend End</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
