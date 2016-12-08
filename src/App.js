cbe937525f535ee58bcd5989ce18d0b1dae30be3
Alex Rolfe <rolfepercussion@gmail.com>
2016-12-07 18:34:13 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
 import './App.css';
+import {fetchData} from './lib/components/Api.js';
 
 class App extends Component {
+
+  // fetchData in componentDidMount
+  // create a componen list reps
   render() {
+    fetchData();
     return (
       <div className="App">
         <h1>Senate and House Contact Information</h1>
