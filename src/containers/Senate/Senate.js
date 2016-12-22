1dde447ee462d5895376c4c205585ccafbdb7c75
Brian Douglas <bdougie@users.noreply.github.com>
2016-12-22 00:51:22 +0000

    add react-transmit (#10)

--- src/containers/Senate/Senate.js
+++ src/containers/Senate/Senate.js
@@ -3,16 +3,7 @@ import { fromJS } from 'immutable'
 import {fetchSenatorData} from '../../lib/democracyApi.js';
 import SenateListComponent from '../../components/ListComponent/ListComponent';
 
-export default class Senate extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {senateData: {}}
-  }
-
-  componentDidMount() {
-    fetchSenatorData().then((response) => this.setState({senateData: response}))
-  }
-
+class Senate extends Component {
   render () {
     // const 0 should be the data I want from the JSON objects
     // what methods can I use on this object to display data?
@@ -25,3 +16,10 @@ export default class Senate extends Component {
     );
   }
 }
+
+export default createContainer(House, {
+  initialVariables: {},
+  fragments: {
+    senateData: () => fetchSenatorData().then(res => res),
+  }
+});
