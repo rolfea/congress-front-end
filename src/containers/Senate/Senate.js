86e55a07dd16f11cbb90303db65207c67a67ec67
Alex Rolfe <rolfepercussion@gmail.com>
2016-12-27 09:11:52 -0600

    Fixed merge conflict leftover from rebase

--- src/containers/Senate/Senate.js
+++ src/containers/Senate/Senate.js
@@ -13,9 +13,6 @@ export default class Senate extends Component {
     fetchSenatorData().then((response) => this.setState({senateData: response}))
   }
   render () {
-    // const 0 should be the data I want from the JSON objects
-    // what methods can I use on this object to display data?
-    // also, what can I do with the const xxx {x, y, z} syntax?
     const { senateData } = this.state
     const mappedSenators = fromJS(senateData);
 
@@ -24,13 +21,3 @@ export default class Senate extends Component {
     );
   }
 }
-<<<<<<< HEAD
-=======
-
-export default createContainer(House, {
-  initialVariables: {},
-  fragments: {
-    senateData: () => fetchSenatorData().then(res => res),
-  }
-});
->>>>>>> 1dde447ee462d5895376c4c205585ccafbdb7c75
