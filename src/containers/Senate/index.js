b1fcc5f4caf31990a8417dcb177a5831d295d4f5
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-12-30 22:57:25 -0800

    speeds up data fetching

--- src/containers/Senate/index.js
+++ src/containers/Senate/index.js
@@ -1,32 +1,28 @@
-import React, {Component, PropTypes} from 'react';
+import React, {PropTypes} from 'react';
+import {createContainer} from 'react-transmit';
 import { fromJS } from 'immutable'
 import {fetchSenatorData} from '../../lib/democracyApi.js';
 import {ListComponent} from '../../components/ListComponent';
 
-export default class Senate extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {senateData: {}}
-  }
+const Senate = ({location, senateData}) => {
+  const searchQuery = location.search;
+  const selectedState = searchQuery.split("?state=")[1];
+  const mappedSenators = fromJS(senateData);
 
-  componentDidMount() {
-    fetchSenatorData().then((response) => this.setState({senateData: response}))
-  }
-
-  render () {
-    const searchQuery = this.props.location.search;
-    const selectedState = searchQuery.split("?state=")[1];
-    console.log(selectedState);
-
-    const { senateData } = this.state
-    const mappedSenators = fromJS(senateData);
-
-    return (
-      <ListComponent selectedState={selectedState} mappedData={mappedSenators}/>
-    );
-  }
+  return (
+    <ListComponent selectedState={selectedState} mappedData={mappedSenators}/>
+  );
 }
 
+export default createContainer(Senate, {
+  initialVariables: {},
+  fragments: {
+    senateData: () => fetchSenatorData().then(res => res),
+  }
+});
+
+
 Senate.propTypes = {
-  senateData: PropTypes.object,
+  senateData: PropTypes.array,
+  location: PropTypes.object,
 };
