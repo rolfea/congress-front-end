d092066e9e60d9a198b765e331a013765f3aabca
Alex Rolfe <rolfepercussion@gmail.com>
2017-01-06 21:55:01 -0600

    Adds CSS styling on list component and list items

--- src/components/ListComponent/ListComponent.js
+++ src/components/ListComponent/ListComponent.js
@@ -8,19 +8,28 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 const ListComponent = ({mappedData, selectedState}) => {
   const filteredData = selectedState ? mappedData.filter((item) => item.get('state') === selectedState): mappedData;
   const muiTheme = getMuiTheme({});
+  const listStyle = {
+    margin: 'auto',
+    width: '50%',
+  };
+  const listItemStyle = {
+    boxShadow: '1px 1px 2px 2px #5D73A0',
+  }
 
   return (
     <MuiThemeProvider muiTheme={muiTheme}>
       <div>
-        <List>
+        <List style={listStyle}>
         { filteredData.valueSeq().map((senator, i) => (
-          <ListItem            
+          <ListItem
+            style={listItemStyle}
             className="listedPerson"
             key={senator.get('id')}
             primaryText={
               senator.get('person').get('firstname') + " " + senator.get('person').get('lastname')
             }
             secondaryText={senator.get('description')}
+            secondaryTextLines={2}
             primaryTogglesNestedList={true}
             nestedItems={[
               <ListItem
